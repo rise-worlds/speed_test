23c6b68c1b47b9f3693d08955cfe4355a273b225
rise <rise.worlds@outlook.com>
2022-01-11 23:53:55 +0800

    返回参数解析

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,39 @@
 mod speed_test_structure;
 
 use reqwest;
-use std::collections::HashMap;
+use std::error::Error;
 use std::fmt::format;
 use std::str;
-use serde::{Deserialize};
 
-use speed_test_structure::*;
+pub use speed_test_structure::*;
 
-async fn get_location(url:&str) -> Result<(LocationInfo), reqwest::Error> {
+async fn get_location(url: &str) -> Result<LocationInfo, reqwest::Error> {
     let body = reqwest::get(url).await?.json::<LocationInfo>().await?;
 
-    println!("body = {:?}", body);
-
+    //println!("body = {:?}", body);
     Ok(body)
 }
 
-async fn get_server_list(url: &str) -> Result<([SpeedTestServerInfo]), reqwest::Error> {
-    let body = reqwest::get(url).await?.json().await?;
+async fn get_server_list(url: &str) -> Result<SpeedTestServerInfo, reqwest::Error> {
+    let body = reqwest::get(url).await?.json::<SpeedTestServerInfo>().await?;
 
-    println!("body = {:?}", body);
-
-    let tmp: [SpeedTestServerInfo] = body["data"];
-    Ok(tmp)
+    //println!("body = {:?}", body);
+    Ok(body)
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>>  {
     println!("Hello, world!");
     // tokio::spawn(async move {
     // });
 
-    let location = get_location("https://forge.speedtest.cn/api/location/info").await;
+    let location = get_location("https://forge.speedtest.cn/api/location/info").await?;
     println!("{:?}", location);
 
-    let url = format(format_args!("https://nodes.speedtest.cn/?https=1&browser=1&page=1&lat={}&lon={}&q=", location.lat, location.lon)).as_str();
-    let servers = get_server_list(url).await;
+    let url = format(format_args!("https://nodes.speedtest.cn/?https=1&browser=1&page=1&lat={}&lon={}&q=", location.lat, location.lon));
+    println!("{:?}", url);
+    let servers = get_server_list(url.as_str()).await?;
     println!("{:?}", servers);
+
+    Ok(())
 }
--- src/speed_test_structure.rs
+++ src/speed_test_structure.rs
@@ -1,38 +1,59 @@
 use std::str;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize};
 
 #[derive(Deserialize, Debug)]
 pub struct LocationInfo
 {
-    ip: String,
-    full_ip: String,
-    country: String,
-    country_code: String,
-    province: String,
-    city: String,
-    distinct: String,
-    isp: String,
-    operator: String,
-    lon: String,
-    lat: String,
-    net_str: String,
+    pub ip: String,
+    pub full_ip: String,
+    pub country: String,
+    pub country_code: String,
+    pub province: String,
+    pub city: String,
+    pub distinct: String,
+    pub isp: String,
+    pub operator: String,
+    pub lon: String,
+    pub lat: String,
+    pub net_str: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct SpeedTestServerItem
+{
+    pub id: String,
+    pub active: String,
+    pub https: String,
+    pub cros: String,
+    pub preferred: String,
+    pub host: String,
+    pub ver: String,
+    pub lon: String,
+    pub lat: String,
+    pub distance: i32,
+    #[serde(rename(deserialize = "pingUrl"))]
+    pub ping_url: String,
+    #[serde(rename(deserialize = "downloadUrl"))]
+    pub download_url: String,
+    #[serde(rename(deserialize = "uploadUrl"))]
+    pub upload_url: String,
+    #[serde(rename(deserialize = "websocketUrl"))]
+    pub websocket_url: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct SpeedTestServerMeta
+{
+    pub current_page: i32,
+    pub last_page: i32,
+    pub per_page: i32,
+    pub  total: i32,
+    pub ip: String
 }
 
 #[derive(Deserialize, Debug)]
 pub struct SpeedTestServerInfo
 {
-    id: String,
-    active: String,
-    https: String,
-    cros: String,
-    preferred: String,
-    host: String,
-    ver: String,
-    lon: String,
-    lat: String,
-    distance: i32,
-    pingUrl: String,
-    downloadUrl: String,
-    uploadUrl: String,
-    websocketUrl: String,
+    pub data: Vec<SpeedTestServerItem>,
+    pub meta: SpeedTestServerMeta
 }
